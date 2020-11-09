39bd3e7b083735531fc7dc42ac5591bffa0b00b9
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-11-09 12:22:46 +0100

    Add project files.

---
+++ Witch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    class Witch : ICharacter, IRaiseShield, IShieldGlare, ITeleport
+    {
+        public void Die()
+        {
+            Console.WriteLine("I'm dying");
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine("I'm fighting");
+        }
+
+        public void Heal()
+        {
+            Console.WriteLine("I'm healing");
+        }
+
+        public void RaiseShield()
+        {
+            Console.WriteLine("I'm raising my shield");
+        }
+
+        public void ShieldGlare()
+        {
+            Console.WriteLine("I'm throwing shield glare");
+        }
+
+        public void Teleport(int x, int y)
+        {
+            Console.WriteLine("I'm teleporting to " + x + " " + y);
+        }
+    }
+}
---
+++ Wizard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    class Wizard : ICharacter, ITeleport, IThrowMagicMissile, IThrowFrostNova
+    {
+        public void Die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Heal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Slash()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Teleport(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowFrostNova()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowMagicMissile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
